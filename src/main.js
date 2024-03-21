df790f6312fe6968ec520e6e905ae47202c3b344
Lorenzo Gutierrez <lorenzoplaypereira@gmail.com>
2024-03-21 18:10:07 -0300

    Criação do main e alterações em shows, variables e index

---
+++ src/main.js
@@ -0,0 +1,34 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const buttons = document.querySelectorAll('[data-tab-button]');
+
+    for (let i = 0; i < buttons.length; i++) {
+        buttons[i].addEventListener('click', function(botao) {
+            const abaAlvo = botao.target.dataset.tabButton;
+            const aba = document.querySelector(`[data-tab-id=${abaAlvo}]`);
+            escondeTodasAbas();
+            aba.classList.add('shows__aba--is-active');
+            removeBotaoAtivo();
+            botao.target.classList.add('shows__tabmenu__list--is-active');
+        })
+    }
+})
+
+function removeBotaoAtivo() {
+    const buttons = document.querySelectorAll('[data-tab-button]');
+
+    for (let i = 0; i < buttons.length; i++) {
+        buttons[i].classList.remove('shows__tabmenu__list--is-active');
+    }
+}
+
+function escondeTodasAbas() {
+    const tabsContainer = document.querySelectorAll('[data-tab-id]');
+
+    for (let i = 0; i < tabsContainer.length; i++) {
+        tabsContainer[i].classList.remove('shows__aba--is-active');
+    }
+}
+
+
+
+
