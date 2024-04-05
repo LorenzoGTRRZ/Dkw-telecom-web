4b4de54b3d837dacb75c024080509af7f6946450
Lorenzo Gutierrez <lorenzoplaypereira@gmail.com>
2024-04-05 17:38:07 -0300

    Adicionando animações e alterações em index e *scss.

--- src/main.js
+++ src/main.js
@@ -31,6 +31,15 @@ function escondeTodasAbas() {
     }
 }
 
+let button = document.getElementById("button");
+
+button.addEventListener('mousemove', (e) => {
+    x = e.offsetX;
+    y = e.offsetY;
+    button.style.setProperty('--mouse-x', x + "px");
+    button.style.setProperty('--mouse-y', y + "px");
+});
+
 
 
 
