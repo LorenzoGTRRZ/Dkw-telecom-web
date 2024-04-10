b216e3cb3b6e1c3a9f92aaa69ca1e0f245f5b2d7
Lorenzo Gutierrez <lorenzoplaypereira@gmail.com>
2024-04-10 16:32:32 -0300

    Atualização no carousel.

--- src/main.js
+++ src/main.js
@@ -40,53 +40,52 @@ button.addEventListener('mousemove', (e) => {
     button.style.setProperty('--mouse-y', y + "px");
 });
 
-document.addEventListener("DOMContentLoaded", function() {
-    let nextDom = document.getElementById('next');
-    let prevDom = document.getElementById('prev');
+// carousel
+let nextDom = document.getElementById('next');
+let prevDom = document.getElementById('prev');
 
-    let carouselDom = document.querySelector('.carousel');
-    let sliderDom = carouselDom.querySelector('.carousel__list');
-    let timeRunning = 3000;
-    let timeAutoNext = 7000;
+let carouselDom = document.querySelector('.carousel');
+let SliderDom = carouselDom.querySelector('.carousel .list');
+let thumbnailBorderDom = document.querySelector('.carousel .thumbnail');
+let thumbnailItemsDom = thumbnailBorderDom.querySelectorAll('.item');
+let timeDom = document.querySelector('.carousel .time');
 
+thumbnailBorderDom.appendChild(thumbnailItemsDom[0]);
+let timeRunning = 3000;
+let timeAutoNext = 7000;
 
-    let runTimeOut;
-    let runNextAuto = setTimeout(() => {
-        nextDom.click();
-    }, timeAutoNext);
+nextDom.onclick = function(){
+    showSlider('next');    
+}
 
-    nextDom.onclick = function() {
-        showSlider('next');
-    };
-
-    prevDom.onclick = function() {
-        showSlider('prev');
-    };
-
-    function showSlider(type) {
-        let sliderItemsDom = sliderDom.querySelectorAll('.carousel__list__item');
-
-        if (type === 'next') {
-            sliderDom.appendChild(sliderItemsDom[0]);;
-            carouselDom.classList.add('next');
-        } else {
-            sliderDom.prepend(sliderItemsDom[sliderItemsDom.length - 1]);
-            carouselDom.classList.add('prev');
-        }
-        
-        clearTimeout(runTimeOut);
-        runTimeOut = setTimeout(() => {
-            carouselDom.classList.remove('next');
-            carouselDom.classList.remove('prev');
-        }, timeRunning);
-
-        clearTimeout(runNextAuto);
-        runNextAuto = setTimeout(() => {
-            nextDom.click();
-        }, timeAutoNext);
+prevDom.onclick = function(){
+    showSlider('prev');    
+}
+let runTimeOut;
+let runNextAuto = setTimeout(() => {
+    next.click();
+}, timeAutoNext)
+function showSlider(type){
+    let  SliderItemsDom = SliderDom.querySelectorAll('.carousel .list .item');
+    let thumbnailItemsDom = document.querySelectorAll('.carousel .thumbnail .item');
+    
+    if(type === 'next'){
+        SliderDom.appendChild(SliderItemsDom[0]);
+        thumbnailBorderDom.appendChild(thumbnailItemsDom[0]);
+        carouselDom.classList.add('next');
+    }else{
+        SliderDom.prepend(SliderItemsDom[SliderItemsDom.length - 1]);
+        thumbnailBorderDom.prepend(thumbnailItemsDom[thumbnailItemsDom.length - 1]);
+        carouselDom.classList.add('prev');
     }
-});
+    clearTimeout(runTimeOut);
+    runTimeOut = setTimeout(() => {
+        carouselDom.classList.remove('next');
+        carouselDom.classList.remove('prev');
+    }, timeRunning);
 
-
-
-
+    clearTimeout(runNextAuto);
+    runNextAuto = setTimeout(() => {
+        next.click();
+    }, timeAutoNext)
+}
