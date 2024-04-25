75941870cdfba2cada3148cda079588480e787dd
Lorenzo Gutierrez <lorenzoplaypereira@gmail.com>
2024-04-25 12:44:18 -0300

    Alterações em produtos

--- content/Produto/Suporte Dielétrico Cônico/product.js
+++ content/Produto/Suporte Dielétrico Cônico/product.js
@@ -2,7 +2,15 @@ const image = document.getElementById('productImg');
 const btn = document.getElementsByClassName('main-wrap__product__image-gallery__controls__btn');
 
 btn[0].addEventListener('click', function(){
-    image.src = '../../dist/images/0-removebg-preview.png';
+    image.src = '../../../dist/images/suporte dielétrico.png';
+    for( bt of btn){
+        bt.classList.remove('main-wrap__product__image-gallery__controls__btn--is-active');
+    }
+    this.classList.add('main-wrap__product__image-gallery__controls__btn--is-active');
+});
+
+btn[1].addEventListener('click', function(){
+    image.src = '../../../dist/images/Suporte Dielétrico Cônico (2).jpg';
     for( bt of btn){
         bt.classList.remove('main-wrap__product__image-gallery__controls__btn--is-active');
     }
